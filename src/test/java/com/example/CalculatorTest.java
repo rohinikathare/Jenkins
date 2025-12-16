76257e6e96fd83c0add7147c3a5342d7cf13c368
rohinikathare <105001605+rohinikathare@users.noreply.github.com>
2025-12-16 13:09:12 +0530

    Create CalculatorTest.java

---
+++ src/test/java/com/example/CalculatorTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAdd() {
+        Calculator calculator = new Calculator();
+        assertEquals(5, calculator.add(2, 3));
+    }
+}
